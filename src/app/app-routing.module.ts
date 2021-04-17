eb91982751c12f474e73faf85aef03cb38fb75e5
takahiroPG <hira.ssr.88@gmail.com>
2021-04-18 02:51:12 +0900

    モーダルが反応しない

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Css1ComponentComponent } from './css1-component/css1-component.component'
+import { Css2ComponentComponent } from './css2-component/css2-component.component'
+import { Modal1Component } from './modal1/modal1.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/css1', pathMatch: 'full' },
+  { path: 'css1', component: Css1ComponentComponent},
+  { path: 'css2', component: Css2ComponentComponent},
+  { path: 'modal1', component: Modal1Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ModalController } from '@ionic/angular';
+import { AngularDelegate } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Css1ComponentComponent } from './css1-component/css1-component.component';
+import { Css2ComponentComponent } from './css2-component/css2-component.component';
+import { Modal1Component } from './modal1/modal1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Css1ComponentComponent,
+    Css2ComponentComponent,
+    Modal1Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ModalController,
+    AngularDelegate
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
